62b753060da808623f80febbe7e919fd8cba66d2
danilomayer <danilomayer2020@gmail.com>
2025-11-22 16:01:53 -0300

    Refactor product pages and implement new product listing feature
    - Updated index.html for product pages to improve structure and accessibility.
    - Removed unnecessary header elements and added a new product detail section.
    - Modified vite.config.js to streamline build process and update input paths.
    - Added new SVG images for product categories: backpacks, hammocks, sleeping bags, and tents.
    - Created CheckoutProcess and ExternalServices modules for handling checkout logic and API interactions.
    - Implemented ProductList module to dynamically render product listings based on category.
    - Developed product-listing.js to initialize product listing and load header/footer.
    - Created index.html for product listing page with appropriate structure and scripts.

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,11 @@
-import { getParam } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData("tents");
+loadHeaderFooter();
+
+const dataSource = new ExternalServices("tents");
 const productID = getParam("product");
 
 const product = new ProductDetails(productID, dataSource);
 product.init();
-
-// // add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// // add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
