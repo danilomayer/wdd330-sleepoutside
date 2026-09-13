38e1e273effea6ea55e625e4b5c48641636e85ae
danilomayer <danilomayer2020@gmail.com>
2026-09-12 22:03:10 -0300

    feat: Update product pages and enhance product details rendering

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -13,7 +13,8 @@ export default class ProductData {
   }
   getData() {
     return fetch(this.path)
-      .then(convertToJson).then((data) => data);
+      .then(convertToJson)
+      .then((data) => data);
   }
   async findProductById(id) {
     const products = await this.getData();
