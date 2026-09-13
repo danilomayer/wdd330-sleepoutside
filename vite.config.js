a423a6e91a756086ecffae38b96a7c132c7f4394
danilomayer <danilomayer2020@gmail.com>
2026-09-12 23:58:01 -0300

    fix: Correct stylesheet and image paths in index.html; update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "./",
   root: "src/",
 
   build: {
@@ -11,6 +12,7 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
+        productDetail: resolve(__dirname, "src/product_pages/index.html"),
         product1: resolve(
           __dirname,
           "src/product_pages/cedar-ridge-rimrock-2.html",
